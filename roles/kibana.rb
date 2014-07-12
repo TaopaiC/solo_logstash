6e757550c32e5cb2b2c7dda02df9bbf4cdc86370
TaopaiC <pctao.blog@pctao.org>
2014-07-13 03:45:08 +0800

    update kibana panel_names

--- roles/kibana.rb
+++ roles/kibana.rb
@@ -14,6 +14,13 @@ default_attributes(
       'type' => 'zip',
       'url' => 'https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.zip',
       'checksum' => '12fd7ed0b1b7857dc0777946daf6858c99099713493f184aeabc6704e23f2916'
+    },
+    'config' => {
+      'panel_names' => [
+        'histogram', 'map', 'pie', 'table', 'filtering', 'timepicker', 'text', 'fields',
+        'hits', 'dashcontrol','column', 'derivequeries', 'trends', 'bettermap', 'query', 'terms',
+        'stats', 'goal', 'sparklines'
+      ]
     }
   }
 )
